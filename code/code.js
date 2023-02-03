efdb14d504a4f527ef393699c708e67378542874
jhonhuanacunim <78872120+jhonhuanacunim@users.noreply.github.com>
2023-02-02 21:50:42 -0500

    Add files via upload

--- code/code.js
+++ code/code.js
@@ -3,3 +3,6 @@ song_te_mato.src = "../src/audios/te_mato.mp3";
 
 var song_que_me_importa = new Audio();
 song_que_me_importa.src = "../src/audios/que_me_importa.mp3";
+
+var song_curso = new Audio();
+song_curso.src = "../src/audios/curso.mp3";
