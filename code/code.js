3e4a244d606a3af309962b1ebd6694e0001ad021
jhonhuanacunim <jhonhuanacunim@unjbg.edu.pe>
2023-09-22 23:37:21 -0500

    agrendo nuevos audios y mejorando fondo

--- code/code.js
+++ code/code.js
@@ -6,3 +6,54 @@ song_que_me_importa.src = "../src/audios/que_me_importa.mp3";
 
 var song_curso = new Audio();
 song_curso.src = "../src/audios/curso.mp3";
+
+var song_curso = new Audio();
+song_curso.src = "../src/audios/curso.mp3";
+
+var song_damelo = new Audio();
+song_damelo.src = "../src/audios/damelo.mp3";
+
+var song_amanecercomotu = new Audio();
+song_amanecercomotu.src = "../src/audios/amanecercomotu.mp3";
+
+var song_becado = new Audio();
+song_becado.src = "../src/audios/becado.mp3";
+
+var song_caldode = new Audio();
+song_caldode.src = "../src/audios/caldode.mp3";
+
+var song_chamo = new Audio();
+song_chamo.src = "../src/audios/chamo.mp3";
+
+var song_ingeniero = new Audio();
+song_ingeniero.src = "../src/audios/ingeniero.mp3";
+
+var song_oeingeniero = new Audio();
+song_oeingeniero.src = "../src/audios/oeingeniero.mp3";
+
+var song_leder = new Audio();
+song_leder.src = "../src/audios/leder.mp3";
+
+var song_pelele = new Audio();
+song_pelele.src = "../src/audios/pelele.mp3";
+
+var song_perro = new Audio();
+song_perro.src = "../src/audios/perro.mp3";
+
+var song_retirate = new Audio();
+song_retirate.src = "../src/audios/retirate.mp3";
+
+var song_retiratemr = new Audio();
+song_retiratemr.src = "../src/audios/retiratemr.mp3";
+
+var song_takataka = new Audio();
+song_takataka.src = "../src/audios/takataka.mp3";
+
+var song_tamuerto = new Audio();
+song_tamuerto.src = "../src/audios/tamuerto.mp3";
+
+var song_malaga = new Audio();
+song_malaga.src = "../src/audios/malaga.mp3";
+
+var song_unicompleto = new Audio();
+song_unicompleto.src = "../src/audios/unicompleto.mp3";
